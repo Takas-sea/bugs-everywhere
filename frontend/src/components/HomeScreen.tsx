cf96b649616aba3b26150cd7ed4b9ac91b95bc60
YAMSITA-SIT <cy23233@shibaura-it.ac.jp>
2026-08-29 15:32:51 +0900

    HomeScreenとLoginScreenを更新

--- frontend/src/components/HomeScreen.tsx
+++ frontend/src/components/HomeScreen.tsx
@@ -81,7 +81,7 @@ export const HomeScreen: React.FC<HomeScreenProps> = ({
               whitespace-nowrap
             "
           >
-           MemoriTrip
+           MemoryTrip
           </h1>
 
           {/* Login / Profile */}
